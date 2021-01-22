495c0b692168f542b9f9dbfac663b010e6a4365d
vetordev <vitor13m2003@gmail.com>
2021-01-22 09:11:11 -0300

    feat: limpando caixa de texto após adição de um item

--- src/pages/home/components/todoHeader/index.jsx
+++ src/pages/home/components/todoHeader/index.jsx
@@ -6,16 +6,24 @@ export default function TodoHeader() {
     const todoContext = useContext(TodoContext);
     const [todo, setTodo] = useState(() => "");
 
+
     const handleAddTodo = (e) => {
         e.preventDefault()
         todoContext.dispatch({ type: 'add', payload: todo });
+
+        setTodo("");
     }
 
     return (
         <div>
             <p>Lista de Atividades</p>
             <div >
-                <input type="text" onChange={(e) => { setTodo(e.target.value) }} placeholder="digite para filtrar"></input>
+                <input
+                    type="text"
+                    value={todo}
+                    onChange={(e) => { setTodo(e.target.value) }}
+                    placeholder="digite para filtrar"
+                />
                 <button type="button" onClick={(e) => handleAddTodo(e)}>Adicionar</button>
             </div>
         </div>
